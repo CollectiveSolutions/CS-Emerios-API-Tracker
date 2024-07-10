295f6df7324923ca121bdaf4e095a48cb2c77696
cesar.carcamo <cesar.carcamo@collectivesolution.net>
2024-07-10 15:57:09 -0600

    EM-13 finished listing page for call records

---
+++ CS-Emerios-API-Tracker/Helper/JSExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace CS_Emerios_API_Tracker.Helper
+{
+    public static class JSExtensions
+    {
+        public static ValueTask<object> SaveFileAs(this IJSRuntime js, string filename, byte[] file)
+        {
+            return js.InvokeAsync<object>("saveAsFile",
+                filename,
+                Convert.ToBase64String(file));
+        }
+    }
+}
